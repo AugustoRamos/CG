fd9c829ec535d22c236f0535b0fcd8f55f258826
AugustoRamos <augustohcr@gmail.com>
2016-04-03 23:08:37 -0300

    Measures and Bresenham for circles

--- TrabalhoCG1/main.cpp
+++ TrabalhoCG1/main.cpp
@@ -6,24 +6,29 @@ using namespace std;
 // Created by augustochaves on 03/04/16.
 //
 
-#include <iostream>
-#include <stdio.h>
 #include <GL/glut.h>
 #include <math.h>
 
 using namespace std;
 
-int x1, Y1, x2, y2, dx, dy, iter;
+int x1, Y1, x2, y2, dx, dy, iter, radius, xCenter, yCenter;
 
+void setPixel(int x, int y){
+
+    glBegin(GL_POINTS);
+    glVertex2i(x,y);
+    glEnd();
+    glFlush();
+}
 
 void bresenhamLine(void){
+
     double dx = abs(x2-x1);
     double dy = abs(y2-Y1);
     double d = 2*dy-dx;
     int const1 = 2*dy;
     int const2 = 2 * (dy-dx);
     int xEnd = 0, y =0, x=0;
-
 
     if(x1 > x2){
         x = x2;
@@ -38,7 +43,7 @@ void bresenhamLine(void){
 
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POINTS);
-    glVertex2d(x,y);
+    setPixel(x,y);
 
     for (int i = 0; i < xEnd; ++i) {
         x = x+1;
@@ -49,13 +54,14 @@ void bresenhamLine(void){
             y = y+1;
             d = d+const2;
         }
-        glVertex2d(x,y);
+        setPixel(x,y);
     }
 
     glEnd();
 
     glFlush();
 }
+
 void dda(void){
 
     double x_inc, y_inc, x, y;
@@ -75,12 +81,50 @@ void dda(void){
 
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_POINTS);
-    glVertex2d(x,y);
+    setPixel(x,y);
 
     for (int i = 0; i < iter; ++i) {
         x = x+x_inc;
         y = y+y_inc;
-        glVertex2d(round(x), round(y));
+        setPixel(round(x), round(y));
+    }
+
+    glEnd();
+    glFlush();
+}
+
+
+void setCirclePixel(int xCentro, int yCentro, int x, int y){
+
+    setPixel(xCentro+x, yCentro+y);
+    setPixel(xCentro+y, yCentro+x);
+    setPixel(xCentro+y, yCentro-x);
+    setPixel(xCentro+x, yCentro-y);
+    setPixel(xCentro-x, yCentro-y);
+    setPixel(xCentro-y, yCentro-x);
+    setPixel(xCentro-y, yCentro+x);
+    setPixel(xCentro-x, yCentro+y);
+}
+
+void bresenhamCircle(void){
+
+    int x = 0, y = radius, d;
+    d = 1-radius;
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POINT);
+    setCirclePixel(xCenter, yCenter, x, y);
+
+    while(x < y){
+        if(d < 0)
+            d = d + 2 * x + 3;
+        else{
+            d = d + 2 * (x-y) + 5;
+            y = y-1;
+        }
+
+        x = x+1;
+        setCirclePixel(xCenter, yCenter, x, y);
     }
 
     glEnd();
@@ -95,34 +139,11 @@ void Init()
     /* Set fill color to black */
     glColor3f(0.0,0.0,0.0);
     glViewport(0 , 0 , 640 , 480);
-    glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0 , 640 , 0 , 480);
 }
 
-int main(int argc, char **argv)
-{
-    int option;
-
-    cout << ("Augusto Chaves Ramos e Pedro Hippolito") << endl;
-    cout << ("-----------------------------------------------");
-    cout << ("Entre com os pontos da linha a ser desenhada\n");
-
-    cout << ("\nX1:\n") << endl;
-    cin >> x1;
-    cout << ("\ny1:\n") << endl;
-    cin >> Y1;
-    cout << "\n-----------------------------------------------";
-    cout << ("\nX2:\n") << endl;
-    cin >> x2;
-    cout << ("\ny2:\n") << endl;
-    cin >> y2;
-
-    cout << "Opções" << endl;
-    cout << "1 - DDA" << endl;
-    cout << "2 - Bresenham para linhas" << endl;
-    cout << "3 - Bresenham para circuferências" << endl;
-    cin >> option;
+void configureGL(int argc, char **argv){
 
     /* Initialise GLUT library */
     glutInit(&argc,argv);
@@ -135,14 +156,66 @@ int main(int argc, char **argv)
     glutCreateWindow("Visualização");
     /* Initialize drawing colors */
     Init();
+}
+
+void getLineCoordinates(){
+
+    cout << ("Entre com os pontos da linha a ser desenhada\n");
+    cout << ("\nX1:\n") << endl;
+    cin >> x1;
+    cout << ("\ny1:\n") << endl;
+    cin >> Y1;
+    cout << "\n-----------------------------------------------";
+    cout << ("\nX2:\n") << endl;
+    cin >> x2;
+    cout << ("\ny2:\n") << endl;
+    cin >> y2;
+}
+
+void getCircleCoordinates(){
+
+    cout << ("Entre com os pontos da circunferência a ser desenhada\n");
+    cout << ("\nX Centro:\n") << endl;
+    cin >> xCenter;
+    cout << ("\ny Centro:\n") << endl;
+    cin >> yCenter;
+    cout << ("\nRaio:\n") << endl;
+    cin >> radius;
+}
+
+int main(int argc, char **argv)
+{
+    int option;
+
+    cout << ("\n\n--------UNESP Rio Claro---------") << endl;
+    cout << ("Ciências da computação") << endl;
+    cout << ("Commputação Gráfica - Noturno 2016") << endl;
+    cout << ("\nAugusto Chaves Ramos") << endl;
+    cout << ("Pedro Hippólito") << endl;
+
+    cout << ("-----------------------------------------------");
+
+    cout << "Opções" << endl;
+    cout << "1 - DDA" << endl;
+    cout << "2 - Bresenham para linhas" << endl;
+    cout << "3 - Bresenham para circuferências" << endl;
+    cin >> option;
 
     switch (option){
         case 1:
+            getLineCoordinates();
+            configureGL(argc, argv);
             glutDisplayFunc(dda);
             break;
         case 2:
+            getLineCoordinates();
+            configureGL(argc, argv);
             glutDisplayFunc(bresenhamLine);
             break;
+        case 3:
+            getCircleCoordinates();
+            configureGL(argc, argv);
+            glutDisplayFunc(bresenhamCircle);
         default:
             cout << ("Opção inválida");
             break;
@@ -150,6 +223,7 @@ int main(int argc, char **argv)
 
     /* Keep displaying untill the program is closed */
     glutMainLoop();
+
 
     return 0;
 }
